0fadb3fc2eaca77d84413b24626193266af3a209
TylerBFabela <hamsterholic777@gmail.com>
2025-03-29 14:00:55 -0700

    Added draw functions to WorldView
    might be wrong, will double check soon

--- src/simstation/WorldView.java
+++ src/simstation/WorldView.java
@@ -3,10 +3,13 @@ package simstation;
 import mvc.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.util.*;
 
 public class WorldView extends View
 {
+    protected static int DIAM = 10;
+
     public WorldView(Model model)
     {
         super(model);
@@ -15,11 +18,19 @@ public class WorldView extends View
 
     public void paintComponent(Graphics gc)
     {
-        // TODO
+        Iterator<Agent> iter = this.model.getWorldAgents().iterator();
+
+        while (iter.hasNext()) // loops through the list
+        {
+            this.drawAgent(iter.next(), gc);
+        }
     }
 
     public void drawAgent(Agent a, Graphics gc)
     {
-        // TODO
+        Color oldColor = gc.getColor();
+        gc.setColor(Color.RED);
+        gc.drawOval(a.getX(), a.getY(), DIAM, DIAM);
+        gc.setColor(oldColor);
     }
 }
